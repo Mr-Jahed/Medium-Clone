fc4f67c1ff43e525052eb038e631169c360aa05f
Jahed Inamdar <jahedinamdar786@gmail.com>
2026-01-13 16:57:13 +0530

    Clean main branch - keep only README.md for production

--- backend/apps/comments/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Comment
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['author', 'article', 'created_at']
-    list_filter = ['created_at']
--- backend/config/settings/prod.py
+++
@@ -1,5 +0,0 @@
-from .base import *
-
-DEBUG = False
-
-# Production-specific settings will be added later
